ef15723c0c72a81cd8b439a2f9dfa5eaf395f685
hankyeolyu <lunia8753@naver.com>
2024-01-16 12:16:42 +0900

    first commit

---
+++ ex1.py
@@ -0,0 +1,26 @@
+from math import *
+
+def solution(s):
+    answer = 0
+    index = len(s) - 1
+    m = 1
+    while index > 0:
+        answer += int(s[index]) * m
+        m *= 10
+        index -= 1
+    if s[0].isdigit():
+        answer += int(s[0]) * m
+    else:
+        if s[0] =='+':
+            pass
+        else:
+            answer *= -1
+    return answer
+
+def main():
+    s = input()
+    answer = solution(s)
+    print(answer)
+    
+if __name__== "__main__":
+    main()
